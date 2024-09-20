dbc4af2626487664125239fbe260148042d0faf7
postwo <codo7717@naver.com>
2024-09-20 21:00:15 +0900

    # thymeleaf 3 decoupled logic 설정
    java code 로 thymeleaf 3 decoupled logic 을
    사용자 application property 로 제어할 수 있게
    설정 추가

--- src/main/java/com/example/JpaBoard/JpaBoardApplication.java
+++ src/main/java/com/example/JpaBoard/JpaBoardApplication.java
@@ -2,8 +2,11 @@ package com.example.JpaBoard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class JpaBoardApplication {
 
 	public static void main(String[] args) {
