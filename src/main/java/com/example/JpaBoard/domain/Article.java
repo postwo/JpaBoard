6acd678b7d37169f2d149691bd2c09c2ce71fa57
postwo <codo7717@naver.com>
2024-09-29 11:49:13 +0900

    #도메인 수정 - 회원 계정과 게시글, 댓글 관계 entity들에 연결

--- src/main/java/com/example/JpaBoard/domain/Article.java
+++ src/main/java/com/example/JpaBoard/domain/Article.java
@@ -10,7 +10,7 @@ import java.util.Objects;
 import java.util.Set;
 
 @Getter
-@ToString
+@ToString(callSuper = true) //callSuper = true는 AuditingFields에 있는거까지 찍겠다는 의미이다
 @Table(indexes = {
         @Index(columnList = "title"),
         @Index(columnList = "hashtag"),
@@ -25,6 +25,8 @@ public class Article extends AuditingFields{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Setter @ManyToOne(optional = false) private UserAccount userAccount; // 유저 정보 (ID)
+
     @Setter @Column(nullable = false) private String title; // 제목
 
     @Setter @Column(nullable = false, length = 10000) private String content; // 본문
@@ -34,21 +36,22 @@ public class Article extends AuditingFields{
     /* 이걸 건 이유는 순환 참조를 막기위해서다(tostring을 끊어 버린다) == 순환참조란 Article entity에서 Tostring이 있는데
     여기 entity에서도 tostring이 있기 때문에 계속 무한 참조가 되는 현상을 말한다.*/
     @ToString.Exclude
-    @OrderBy("id")
+    @OrderBy("createdAt DESC")
     @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
     private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
 
 
     protected Article() {}
 
-    private Article(String title, String content, String hashtag) {
+    private Article(UserAccount userAccount, String title, String content, String hashtag) {
+        this.userAccount = userAccount;
         this.title = title;
         this.content = content;
         this.hashtag = hashtag;
     }
 
-    public static Article of(String title, String content, String hashtag) {
-        return new Article(title, content, hashtag);
+    public static Article of(UserAccount userAccount, String title, String content, String hashtag) {
+        return new Article(userAccount, title, content, hashtag);
     }
 
 
--- src/test/java/com/example/JpaBoard/repository/JpaRepositoryTest.java
+++ src/test/java/com/example/JpaBoard/repository/JpaRepositoryTest.java
@@ -2,6 +2,7 @@ package com.example.JpaBoard.repository;
 
 import com.example.JpaBoard.config.JpaConfig;
 import com.example.JpaBoard.domain.Article;
+import com.example.JpaBoard.domain.UserAccount;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,13 +21,16 @@ class JpaRepositoryTest {
 
     private final ArticleRepository articleRepository;
     private final ArticleCommentRepository articleCommentRepository;
+    private final UserAccountRepository userAccountRepository;
 
     public JpaRepositoryTest(
             @Autowired ArticleRepository articleRepository,
-            @Autowired ArticleCommentRepository articleCommentRepository
+            @Autowired ArticleCommentRepository articleCommentRepository,
+            @Autowired UserAccountRepository userAccountRepository
     ) {
         this.articleRepository = articleRepository;
         this.articleCommentRepository = articleCommentRepository;
+        this.userAccountRepository = userAccountRepository;
     }
 
     @DisplayName("select 테스트")
@@ -48,9 +52,11 @@ class JpaRepositoryTest {
     void givenTestData_whenInserting_thenWorksFine() {
         // Given
         long previousCount = articleRepository.count();
+        UserAccount userAccount = userAccountRepository.save(UserAccount.of("uno", "pw", null, null, null));
+        Article article = Article.of(userAccount, "new article", "new content", "#spring");
 
         // When
-        Article savedArticle = articleRepository.save(Article.of("new article", "new content", "#spring"));
+        articleRepository.save(article);
 
         // Then
         assertThat(articleRepository.count()).isEqualTo(previousCount + 1);
