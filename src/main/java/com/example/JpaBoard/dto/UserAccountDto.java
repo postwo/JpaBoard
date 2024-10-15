fda6e6f104696b0f76b6775503d2e6bc47d2e727
postwo <codo7717@naver.com>
2024-10-15 15:13:04 +0900

    # 회원 계정 도메인 변경사항을 영향받는 코드에 적용
    `id` -> `userId`로 id를 변경하는 내용을
    모든 자바코드에 반영

--- src/main/java/com/example/JpaBoard/dto/UserAccountDto.java
+++ src/main/java/com/example/JpaBoard/dto/UserAccountDto.java
@@ -5,7 +5,6 @@ import com.example.JpaBoard.domain.UserAccount;
 import java.time.LocalDateTime;
 
 public record UserAccountDto(
-        Long id,
         String userId,
         String userPassword,
         String email,
@@ -17,14 +16,13 @@ public record UserAccountDto(
         String modifiedBy
 ) {
 
-    public static UserAccountDto of(Long id, String userId, String userPassword, String email, String nickname, String memo, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
-        return new UserAccountDto(id, userId, userPassword, email, nickname, memo, createdAt, createdBy, modifiedAt, modifiedBy);
+    public static UserAccountDto of(String userId, String userPassword, String email, String nickname, String memo, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        return new UserAccountDto(userId, userPassword, email, nickname, memo, createdAt, createdBy, modifiedAt, modifiedBy);
     }
 
 
     public static UserAccountDto from(UserAccount entity) {
         return new UserAccountDto(
-                entity.getId(),
                 entity.getUserId(),
                 entity.getUserPassword(),
                 entity.getEmail(),
--- src/main/java/com/example/JpaBoard/repository/UserAccountRepository.java
+++ src/main/java/com/example/JpaBoard/repository/UserAccountRepository.java
@@ -3,5 +3,5 @@ package com.example.JpaBoard.repository;
 import com.example.JpaBoard.domain.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
 }
