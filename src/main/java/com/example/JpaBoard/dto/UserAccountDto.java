e33a0a05de2a42fffbb0f05d0406a42617daf843
postwo <codo7717@naver.com>
2024-10-22 12:46:58 +0900

    useraccountDto에서 잘못 삭제한 staticle 메서드를 다시 추가

--- src/main/java/com/example/JpaBoard/dto/UserAccountDto.java
+++ src/main/java/com/example/JpaBoard/dto/UserAccountDto.java
@@ -20,6 +20,9 @@ public record UserAccountDto(
         return new UserAccountDto(userId, userPassword, email, nickname, memo, null, null, null, null);
     }
 
+    public static UserAccountDto of(String userId, String userPassword, String email, String nickname, String memo, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        return new UserAccountDto(userId, userPassword, email, nickname, memo, createdAt, createdBy, modifiedAt, modifiedBy);
+    }
 
     public static UserAccountDto from(UserAccount entity) {
         return new UserAccountDto(
