c9e184868a458cfbf83cc09429eae043bc65c71f
postwo <codo7717@naver.com>
2024-10-30 22:30:12 +0900

    #중복 표현 삭제
    들여쓰기가 다른 곳과 다르게 된 부분 수정.
    검사 구문이 중복으로 들어간 곳이 있다. 편집 실수인 듯.
    중복 표현도 삭제

--- src/test/java/com/example/JpaBoard/controller/ArticleControllerTest.java
+++ src/test/java/com/example/JpaBoard/controller/ArticleControllerTest.java
@@ -168,7 +168,6 @@ class ArticleControllerTest {
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments")) //댓글(1개 or 여러개)도 추가
-                .andExpect(model().attributeExists("articleComments"))
                 .andExpect(model().attribute("totalCount", totalCount));
         then(articleService).should().getArticleWithComments(articleId);
         then(articleService).should().getArticleCount();
