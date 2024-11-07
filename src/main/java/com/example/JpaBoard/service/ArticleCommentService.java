20495accc58ebcdb77224917a825e837258ee686
postwo <codo7717@naver.com>
2024-11-07 13:39:21 +0900

    #댓글 업데이트 기능의 삭제
    왜 불필요한지는 지난 javadoc 기록으로 잘 정리되어 있었음
    다른 곳에서 사용하지 않았기 때문에
    기능과 테스트의 깔끔한 분리 제거가 가능했다.

--- src/main/java/com/example/JpaBoard/service/ArticleCommentService.java
+++ src/main/java/com/example/JpaBoard/service/ArticleCommentService.java
@@ -1,7 +1,6 @@
 package com.example.JpaBoard.service;
 
 import com.example.JpaBoard.domain.Article;
-import com.example.JpaBoard.domain.ArticleComment;
 import com.example.JpaBoard.domain.UserAccount;
 import com.example.JpaBoard.dto.ArticleCommentDto;
 import com.example.JpaBoard.repository.ArticleCommentRepository;
@@ -42,19 +41,6 @@ public class ArticleCommentService {
             articleCommentRepository.save(dto.toEntity(article, userAccount));
         } catch (EntityNotFoundException e) {
             log.warn("댓글 저장 실패. 댓글 작성에 필요한 정보를 찾을 수 없습니다 - {}", e.getLocalizedMessage());
-        }
-    }
-
-    /**
-     * @deprecated 댓글 수정 기능은 클라이언트에서 생각할 점이 많아지기 떄문에, 이번 개발에서는 제공하지 않기로 했다.
-     */
-    @Deprecated
-    public void updateArticleComment(ArticleCommentDto dto) {
-        try {
-            ArticleComment articleComment = articleCommentRepository.getReferenceById(dto.id());
-            if (dto.content() != null) { articleComment.setContent(dto.content()); }
-        } catch (EntityNotFoundException e) {
-            log.warn("댓글 업데이트 실패. 댓글을 찾을 수 없습니다 - dto: {}", dto);
         }
     }
 
