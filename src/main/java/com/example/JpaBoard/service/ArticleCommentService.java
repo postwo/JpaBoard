98ec70878718b73721bfea4db322d13ba24b07c6
DESKTOP-IS6PFV9\codo7 <codo7717@naver.com>
2024-11-09 21:25:01 +0900

    # 대댓글 비즈니스 로직 구현
    부모/자식 관계를 만들면서 entity <-> dto 간 변환 로직,
    댓글 서비스에서 댓글과 대댓글을 저장하고 삭제하는 방법,
    댓글과 대댓글을 db 데이터로부터 변환할 때
    서로 계층 구조를 만들고 각각 정렬하는 과정을 구현

--- src/main/java/com/example/JpaBoard/service/ArticleCommentService.java
+++ src/main/java/com/example/JpaBoard/service/ArticleCommentService.java
@@ -1,6 +1,7 @@
 package com.example.JpaBoard.service;
 
 import com.example.JpaBoard.domain.Article;
+import com.example.JpaBoard.domain.ArticleComment;
 import com.example.JpaBoard.domain.UserAccount;
 import com.example.JpaBoard.dto.ArticleCommentDto;
 import com.example.JpaBoard.repository.ArticleCommentRepository;
@@ -38,7 +39,14 @@ public class ArticleCommentService {
         try {
             Article article = articleRepository.getReferenceById(dto.articleId());
             UserAccount userAccount = userAccountRepository.getReferenceById(dto.userAccountDto().userId());
-            articleCommentRepository.save(dto.toEntity(article, userAccount));
+
+            ArticleComment articleComment = dto.toEntity(article, userAccount);
+            if (dto.parentCommentId() != null) {
+                ArticleComment parentComment = articleCommentRepository.getReferenceById(dto.parentCommentId());
+                parentComment.addChildComment(articleComment);
+            } else {
+                articleCommentRepository.save(articleComment);
+            }
         } catch (EntityNotFoundException e) {
             log.warn("댓글 저장 실패. 댓글 작성에 필요한 정보를 찾을 수 없습니다 - {}", e.getLocalizedMessage());
         }
